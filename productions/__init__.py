6c50978776d75a74b34f759fc1167ba761fca0b9
wlpinjlte <115573646+wlpinjlte@users.noreply.github.com>
2026-01-16 14:27:35 +0100

    feat: add P2, P6 (#3)
    * add p6 production
    
    * add p2 production
    
    * add p6 example in large graph
    
    * remove p2 outputs folder
    
    ---------
    
    Co-authored-by: Witold Strzeboński <wstrzebo@gmail.com>

--- productions/__init__.py
+++ productions/__init__.py
@@ -2,6 +2,8 @@
 
 from productions.production_base import Production
 from productions.p0.p0 import P0
+from productions.p2.p2 import P2
+from productions.p6.p6 import P6
 from productions.p9.p9 import P9
 from productions.p10.p10 import P10
 from productions.p3.p3 import P3
@@ -11,4 +13,4 @@ from productions.p12.p12 import P12
 from productions.p1.p1 import P1
 from productions.p5.p5 import P5
 
-__all__ = ['Production', 'P0', 'P1' 'P3', 'P5', 'P7', 'P9', 'P10', 'P11', 'P12']
+__all__ = ['Production', 'P0', 'P1', 'P2', 'P3', 'P5', 'P6', 'P7', 'P9', 'P10', 'P11', 'P12']
